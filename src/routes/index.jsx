a6c40bf48952d569a114ae505084e915fd68aa5b
Marcola <sbmarcos777@gmail.com>
2022-11-16 22:08:05 -0300

    Primeira parte das telas motorista e passageiro

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,7 @@ import { Login } from '../pages/Login';
 import { Cadastro } from '../pages/Cadastro';
 import { CadastroCarro } from '../pages/CadastroCarro';
 import { CadastroEndereco } from '../pages/CadastroEndereco';
+import { Passageiro} from '../pages/Passageiro';
 
 export const AppRouter = () =>{
     return(
@@ -13,6 +14,7 @@ export const AppRouter = () =>{
                 <Route path="/cadastro" exact element={<Cadastro />} />
                 <Route path="/cadastro_carro" exact element={<CadastroCarro />} />
                 <Route path="/cadastro_endereco" exact element={<CadastroEndereco />} />
+                <Route path="/passageiro" exact element={<Passageiro />} />
             </Routes>
         </Router>
     );
