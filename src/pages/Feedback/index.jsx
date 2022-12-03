1295de5a34b8ad166705da23c1f9bfdbfe72eb1d
Argusto Lima <augusto.vlima2@gmai.com>
2022-12-03 15:23:38 -0300

    Desenvolvimento da tela de feedback

--- src/pages/Feedback/index.jsx
+++ src/pages/Feedback/index.jsx
@@ -1,12 +1,41 @@
 import { LayoutSemLoginCadastro } from '../../components/LayoutSemLoginCadastro';
-
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export const Feedback = () => {
+  const [nome, setNome] = useState("")
+  const [feedback, setFeedback] = useState("")
   return (
     <LayoutSemLoginCadastro>
+      <form className="conteudo-principal-escrito">
 
-        <div className='conteudo-principal-escrito-subtitulo flex'>Feedback</div>   
+        <span className="conteudo-principal-escrito-titulo flex"> FeedBack</span>
+        <span className="conteudo-principal-escrito-subtitulo flex"> Insira seu nome e de seu feedback sobre nosso site</span>
 
+        <div className="flex">
+            <input
+                placeholder="Seu nome"
+                className={nome !== "" ? 'has-val input' : 'input'}
+                type="text"
+                value={nome}
+                onChange={e => setNome(e.target.value)}/>
+        </div>
+        
+        <div className="flex">
+            <textarea 
+                placeholder="Comentário"
+                className= 'input text-box' 
+                type="text"
+                value={feedback}
+                onChange={e => setFeedback(e.target.value)}/>
+        </div>
+
+        <footer className="rodape">
+            <p className="rodape-escrito">Developed by</p>
+            <p className="rodape-escrito-nome">Augusto Lima, Emanoel Santos, Gustavo Alves, Marcos Vinicius</p>     
+        </footer>
+
+      </form>
     </LayoutSemLoginCadastro>
   );
 }
